7b9a6d48f088f5db03ced2ee9f7fdec431d8befc
C1330638 Renan Matias Mendes <c1330638@interno.bb.com.br>
2025-03-29 10:11:56 -0300

    init cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../header/header/header.component";
+import { ButtonModule } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-cart',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ButtonModule, StepperModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
